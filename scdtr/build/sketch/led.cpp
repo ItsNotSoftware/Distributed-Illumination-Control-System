4802269224260712782324b6e91e26feaf0ad7ee
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-02-29 03:06:04 +0000

    removed build

--- scdtr/build/sketch/led.cpp
+++
@@ -1,9 +0,0 @@
-#line 1 "/Users/diogoferreira/CodeWs/Distributed-Illumination-Control-System/scdtr/led.cpp"
-#include "led.hpp"
-
-LED::LED(uint8_t pin) : pin(pin) {
-    pinMode(pin, OUTPUT);
-    analogWriteFreq(60000);
-}
-
-void LED::set_pwm_range(uint8_t range) { analogWrite(pin, range); }
